5f42493206aa4fc1aa7b8d00bd2b67cc6a24dcc8
Alessandro Pulcini <alessandro.pulcini0191@gmail.com>
2025-04-24 18:44:25 +0200

    progressi navbar AS Roma

--- src/components/NavbarDown.tsx
+++ src/components/NavbarDown.tsx
@@ -3,6 +3,9 @@ import { asRomaNavbarList } from "../utils/data";
 import hamburgerLogo from "../assets/hamburger.png";
 import sortDown from "../assets/down.png";
 import search from "../assets/searchh.png";
+import ticket from "../assets/ticket.png";
+import shop from "../assets/shopping-cart.png";
+import rightArrow from "../assets/right-arrow.png";
 
 const NavbarDown = () => {
   return (
@@ -22,10 +25,22 @@ const NavbarDown = () => {
         })}
       </div>
       <div className="navbar-down-right">
-        <div className="text">
+        <div className="text-language">
           IT
           <img className="sort-down" src={sortDown} alt="sort down" />
-          <img className="search" src={search} alt="search" />
+        </div>
+        <img className="search" src={search} alt="search" />
+        <div className="text-ticket">
+          <img className="ticket" src={ticket} alt="ticket" />
+          BIGLIETTI
+        </div>
+        <div className="text-shop">
+          <img className="shop" src={shop} alt="shop" />
+          SHOP
+        </div>
+        <div className="text-access">
+          ACCEDI
+          <img className="right-arrow" src={rightArrow} alt="right arrow" />
         </div>
       </div>
     </div>
