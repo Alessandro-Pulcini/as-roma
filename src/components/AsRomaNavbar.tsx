6af0b45f641cb763e363ccb68fa68933d7f5bc8c
Alessandro Pulcini <alessandro.pulcini0191@gmail.com>
2025-05-06 17:31:58 +0200

    carousel

--- src/components/AsRomaNavbar.tsx
+++ src/components/AsRomaNavbar.tsx
@@ -2,9 +2,10 @@ import React from "react";
 import { asRomaNavbarList } from "../utils/data";
 import NavbarUp from "./NavbarUp";
 import NavbarDown from "./NavbarDown";
-
+import ExampleCarouselImage from "./ExampleCarouselImage";
 import roma from "../assets/as-roma.svg";
 import "./asroma.css";
+
 
 const AsRomaNavbar = () => {
   return (
@@ -12,26 +13,7 @@ const AsRomaNavbar = () => {
       <img className="logo-roma" src={roma} alt="roma logo" />
       <NavbarUp />
       <NavbarDown />
-      <div className="snap-x ...">
-        <div className="snap-start ...">
-          <img src="https://images.unsplash.com/photo-1604999565976-8913ad2ddb7c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&h=160&q=80" />
-        </div>
-        <div className="snap-start ...">
-          <img src="https://images.unsplash.com/photo-1540206351-d6465b3ac5c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&h=160&q=80" />
-        </div>
-        <div className="snap-start ...">
-          <img src="https://images.unsplash.com/photo-1622890806166-111d7f6c7c97?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&h=160&q=80" />
-        </div>
-        <div className="snap-start ...">
-          <img src="https://images.unsplash.com/photo-1590523277543-a94d2e4eb00b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&h=160&q=80" />
-        </div>
-        <div className="snap-start ...">
-          <img src="https://images.unsplash.com/photo-1575424909138-46b05e5919ec?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&h=160&q=80" />
-        </div>
-        <div className="snap-start ...">
-          <img src="https://images.unsplash.com/photo-1559333086-b0a56225a93c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&h=160&q=80" />
-        </div>
-      </div>
+      <ExampleCarouselImage />
     </div>
   );
 };
