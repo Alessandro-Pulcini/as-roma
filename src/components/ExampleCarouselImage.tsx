21146f680e84cc08cf3583e485116868089873ea
Alessandro Pulcini <alessandro.pulcini0191@gmail.com>
2025-05-07 13:10:51 +0200

    fixed carousel

--- src/components/ExampleCarouselImage.tsx
+++ src/components/ExampleCarouselImage.tsx
@@ -5,7 +5,7 @@ type Props = {
 };
 
 const ExampleCarouselImage: React.FC<Props> = ({ text }) => (
-  <div className="w-full h-80 bg-gray-300 flex items-center justify-center text-2xl font-bold text-white">
+  <div className="w-full h-svh bg-indigo-500 flex items-center justify-center text-2xl font-bold text-white">
     {text}
   </div>
 );
