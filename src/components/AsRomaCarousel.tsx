6115bb524ce180de8ffe045d44d65ac16d731274
Philip Grasselli <philip.grasselli@gmail.com>
2025-05-13 12:03:56 +0200

    (bug-fix) Sfondo carousel

--- src/components/AsRomaCarousel.tsx
+++ src/components/AsRomaCarousel.tsx
@@ -1,19 +1,21 @@
 import React from "react";
 import Carousel from "react-bootstrap/Carousel";
 import ExampleCarouselImage from "../components/ExampleCarouselImage";
+import asRomaMonster from "../assets/roma-monster.jpg";
+import rissaRomaLazio from "../assets/rissa-roma-lazio.jpg";
 
 function AsRomaCarousel() {
   return (
     <Carousel>
       <Carousel.Item>
-        <ExampleCarouselImage text="First slide" />
+        <ExampleCarouselImage text="First slide" imageUrl={asRomaMonster} />
         <Carousel.Caption>
           <h3>First slide label</h3>
           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <ExampleCarouselImage text="Second slide" />
+        <ExampleCarouselImage text="Second slide" imageUrl={rissaRomaLazio} />
         <Carousel.Caption>
           <h3>Second slide label</h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
--- src/components/ExampleCarouselImage.tsx
+++ src/components/ExampleCarouselImage.tsx
@@ -2,10 +2,15 @@ import React from "react";
 
 type Props = {
   text?: string;
+  imageUrl?: string;
 };
 
-const ExampleCarouselImage: React.FC<Props> = ({ text }) => (
-  <div className="w-full h-svh bg-gray-400 flex items-center justify-center text-2xl font-bold text-white">
+const ExampleCarouselImage: React.FC<Props> = ({ text, imageUrl }) => (
+  <div
+    style={{ backgroundImage: `url(${imageUrl})` }}
+    // className={`w-full h-svh flex items-center justify-center text-2xl font-bold text-white bg-[url("/public/assets/roma-monster.jpg")] bg-cover bg-center bg-no-repeat`}
+    className={`w-full h-svh bg-gray-400 flex items-center justify-center text-2xl font-bold text-white bg-cover bg-center bg-no-repeat`}
+  >
     {text}
   </div>
 );
